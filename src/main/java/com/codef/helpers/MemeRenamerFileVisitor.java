aba005f7b5696661e5dc0b684e4690d0a8ebe75f
SIRFozzieSEA <github@codef.com>
2021-05-27 20:39:46 -0700

    Fixed renamer

--- src/main/java/com/codef/helpers/MemeRenamerFileVisitor.java
+++ src/main/java/com/codef/helpers/MemeRenamerFileVisitor.java
@@ -1,22 +1,27 @@
 package com.codef.helpers;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.TreeSet;
 
+import com.codef.xsalt.utils.XSaLTFileSystemUtils;
 import com.codef.xsalt.utils.XSaLTFileVisitor;
 
 public class MemeRenamerFileVisitor extends XSaLTFileVisitor {
 
+	private static final String sourceFolder = "C:\\_PRIMARY_SORT\\Memes";
+	private static final String targetFolder = "E:\\Memes";
+
 	private static Set<String> folderSet = new TreeSet<>();
 
 	public static void main(String[] args) {
-		
+
 		HashMap<String, String> myArgumentsMap = new HashMap<String, String>();
-		myArgumentsMap.put("PREFIX", "eew");
+		myArgumentsMap.put("PREFIX", "aaa");
 		MemeRenamerFileVisitor myMfr = new MemeRenamerFileVisitor(myArgumentsMap);
-		myMfr.startVisit("C:\\_PRIMARY_SORT\\Memes");
-		
+		myMfr.startVisit(sourceFolder);
+
 	}
 
 	public MemeRenamerFileVisitor(HashMap<String, String> argumentsMap) {
@@ -26,6 +31,8 @@ public class MemeRenamerFileVisitor extends XSaLTFileVisitor {
 	@Override
 	public void visitFileCode(String filePath) {
 
+		int finalFiles = 0;
+		
 		String[] fileParts = filePath.split("\\\\");
 
 		String folderName = fileParts[fileParts.length - 2];
@@ -40,11 +47,25 @@ public class MemeRenamerFileVisitor extends XSaLTFileVisitor {
 		}
 
 		String padNo = String.format("%010d", fileCount);
-		String newFileNameToUse = filePrefixNew + " index " + argumentsMap.get("PREFIX") + padNo + "." + nFileExtension.toLowerCase();
+		String newFileNameToUse = filePrefixNew + " index " + argumentsMap.get("PREFIX") + padNo + "."
+				+ nFileExtension.toLowerCase();
 
 		if (!nFileExtension.toLowerCase().equals("ini") && !nFileExtension.toLowerCase().equals("db")) {
-			String renameFile = "rename \"" + filePath + "\" \"" + newFileNameToUse + "\"";
-			System.out.println(renameFile);
+			
+			finalFiles = finalFiles + 1;
+			
+			String targetFile = filePath.replace(sourceFolder, targetFolder).replace(fileName, newFileNameToUse);
+			System.out.println(finalFiles + " --> from: " + filePath + " to: " + targetFile);
+
+			try {
+				XSaLTFileSystemUtils.copyFile(filePath, targetFile);
+				XSaLTFileSystemUtils.deleteFile(filePath, false);
+			} catch (IOException e) {
+				// e.printStackTrace();
+			}
+
+		
+
 		}
 
 	}
