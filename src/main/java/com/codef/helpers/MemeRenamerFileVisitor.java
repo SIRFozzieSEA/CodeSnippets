78eadd08ab87783dffc0838d20ef925cab491521
SIRFozzieSEA <github@codef.com>
2021-08-11 17:23:54 -0700

    Changed prefix

--- src/main/java/com/codef/helpers/MemeRenamerFileVisitor.java
+++ src/main/java/com/codef/helpers/MemeRenamerFileVisitor.java
@@ -22,7 +22,7 @@ public class MemeRenamerFileVisitor extends XSaLTFileVisitor {
 	public static void main(String[] args) {
 
 		HashMap<String, String> myArgumentsMap = new HashMap<String, String>();
-		myArgumentsMap.put("PREFIX", "bbb");
+		myArgumentsMap.put("PREFIX", "ccc");
 		MemeRenamerFileVisitor myMfr = new MemeRenamerFileVisitor(myArgumentsMap);
 		myMfr.startVisit(sourceFolder);
 
