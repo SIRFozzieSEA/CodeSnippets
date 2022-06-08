3f5f1710a75d3a0f3c5ab44cf391c53b8c2b3ed9
SIRFozzieSEA <github@codef.com>
2022-06-08 15:58:29 -0700

    Prefix change

--- src/main/java/com/codef/helpers/MemeRenamerFileVisitor.java
+++ src/main/java/com/codef/helpers/MemeRenamerFileVisitor.java
@@ -23,7 +23,7 @@ public class MemeRenamerFileVisitor extends XSaLTFileVisitor {
 	public static void main(String[] args) {
 
 		HashMap<String, String> myArgumentsMap = new HashMap<String, String>();
-		myArgumentsMap.put("PREFIX", "kkk");
+		myArgumentsMap.put("PREFIX", "lll");
 		MemeRenamerFileVisitor myMfr = new MemeRenamerFileVisitor(myArgumentsMap);
 		myMfr.startVisit(sourceFolder);
 
