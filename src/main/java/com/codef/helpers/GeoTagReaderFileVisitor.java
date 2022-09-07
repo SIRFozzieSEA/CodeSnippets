9ad37fbe958083107828231b40397ccb7869a673
SIRFozzieSEA <github@codef.com>
2022-09-07 16:58:49 -0700

    Dependabot

--- src/main/java/com/codef/helpers/GeoTagReaderFileVisitor.java
+++ src/main/java/com/codef/helpers/GeoTagReaderFileVisitor.java
@@ -1,22 +1,20 @@
 package com.codef.helpers;
 
-//import java.io.File;
+import java.io.File;
 import java.util.HashMap;
 
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.codef.xsalt.utils.XSaLTFileVisitor;
-//import com.drew.imaging.ImageMetadataReader;
-//import com.drew.metadata.Directory;
-//import com.drew.metadata.Metadata;
-//import com.drew.metadata.Tag;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
 
 public class GeoTagReaderFileVisitor extends XSaLTFileVisitor {
-	
-	// TODO:  Find dependency that isn't compromised
 
-//	private static final Logger LOGGER = LogManager.getLogger(GeoTagReaderFileVisitor.class.getName());
+	private static final Logger LOGGER = LogManager.getLogger(GeoTagReaderFileVisitor.class.getName());
 
 	private static final String sourceFolder = "E:\\Pictures\\Events\\Hiking with Tenzin\\20200404";
 
@@ -31,62 +29,62 @@ public class GeoTagReaderFileVisitor extends XSaLTFileVisitor {
 		myGtrfv.startVisit(sourceFolder);
 
 	}
-//
-//	@Override
-//	public void visitFileCode(String filePath) {
-//		if (filePath.endsWith(".jpg")) {
-//			printCoordinates(filePath);
-//		}
-//	}
-//
-//	public void printCoordinates(String photoFileName) {
-//
-//		try {
-//			File file = new File(photoFileName);
-//
-//			double lastLat = 0;
-//			double lastLong = 0;
-//
-//			LOGGER.info(photoFileName);
-//
-//			Metadata metadata = ImageMetadataReader.readMetadata(file);
-//
-//			for (Directory directory : metadata.getDirectories()) {
-//				for (Tag tag : directory.getTags()) {
-//
-//					if (tag.getTagName().equals("GPS Longitude")) {
-//						String[] parts = tag.getDescription().split(" ");
-//						double degrees = Double.valueOf(parts[0].substring(0, parts[0].length() - 1));
-//						double minutes = Double.valueOf(parts[1].substring(0, parts[1].length() - 1));
-//						double seconds = Double.valueOf(parts[2].substring(0, parts[2].length() - 1));
-//						lastLong = Math.signum(degrees) * (Math.abs(degrees) + (minutes / 60.0) + (seconds / 3600.0));
-//					}
-//
-//					if (tag.getTagName().equals("GPS Latitude")) {
-//						String[] parts = tag.getDescription().split(" ");
-//						double degrees = Double.valueOf(parts[0].substring(0, parts[0].length() - 1));
-//						double minutes = Double.valueOf(parts[1].substring(0, parts[1].length() - 1));
-//						double seconds = Double.valueOf(parts[2].substring(0, parts[2].length() - 1));
-//						lastLat = Math.signum(degrees) * (Math.abs(degrees) + (minutes / 60.0) + (seconds / 3600.0));
-//					}
-//
-//				}
-//				if (directory.hasErrors()) {
-//					for (String error : directory.getErrors()) {
-//						LOGGER.error(error);
-//					}
-//				}
-//			}
-//
-//			String latitude = String.format("%.4f", lastLat);
-//			String longitude = String.format("%.4f", lastLong);
-//			LOGGER.info("\t" + latitude + " " + longitude);
-//
-//		} catch (Exception e) {
-//			LOGGER.error(e.toString());
-//			e.printStackTrace();
-//		}
-//
-//	}
+
+	@Override
+	public void visitFileCode(String filePath) {
+		if (filePath.endsWith(".jpg")) {
+			printCoordinates(filePath);
+		}
+	}
+
+	public void printCoordinates(String photoFileName) {
+
+		try {
+			File file = new File(photoFileName);
+
+			double lastLat = 0;
+			double lastLong = 0;
+
+			LOGGER.info(photoFileName);
+
+			Metadata metadata = ImageMetadataReader.readMetadata(file);
+
+			for (Directory directory : metadata.getDirectories()) {
+				for (Tag tag : directory.getTags()) {
+
+					if (tag.getTagName().equals("GPS Longitude")) {
+						String[] parts = tag.getDescription().split(" ");
+						double degrees = Double.valueOf(parts[0].substring(0, parts[0].length() - 1));
+						double minutes = Double.valueOf(parts[1].substring(0, parts[1].length() - 1));
+						double seconds = Double.valueOf(parts[2].substring(0, parts[2].length() - 1));
+						lastLong = Math.signum(degrees) * (Math.abs(degrees) + (minutes / 60.0) + (seconds / 3600.0));
+					}
+
+					if (tag.getTagName().equals("GPS Latitude")) {
+						String[] parts = tag.getDescription().split(" ");
+						double degrees = Double.valueOf(parts[0].substring(0, parts[0].length() - 1));
+						double minutes = Double.valueOf(parts[1].substring(0, parts[1].length() - 1));
+						double seconds = Double.valueOf(parts[2].substring(0, parts[2].length() - 1));
+						lastLat = Math.signum(degrees) * (Math.abs(degrees) + (minutes / 60.0) + (seconds / 3600.0));
+					}
+
+				}
+				if (directory.hasErrors()) {
+					for (String error : directory.getErrors()) {
+						LOGGER.error(error);
+					}
+				}
+			}
+
+			String latitude = String.format("%.4f", lastLat);
+			String longitude = String.format("%.4f", lastLong);
+			LOGGER.info("\t" + latitude + " " + longitude);
+
+		} catch (Exception e) {
+			LOGGER.error(e.toString());
+			e.printStackTrace();
+		}
+
+	}
 
 }
