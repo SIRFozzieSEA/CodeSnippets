cdd2933f2105afd06a14ba6584b29149ae7a488b
SIRFozzieSEA <github@codef.com>
2023-04-13 22:43:07 -0700

    Logger stuff

---
+++ src/main/java/com/codef/helpers/LogTester.java
@@ -0,0 +1,20 @@
+package com.codef.helpers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogTester {
+	
+	private static final Logger LOGGER = LogManager.getLogger(LogTester.class.getName());
+	
+	public static void main(String[] args) {
+		
+		LOGGER.trace("A TRACE Message");
+		LOGGER.debug("A DEBUG Message");
+		LOGGER.info("An INFO Message");
+		LOGGER.warn("A WARN Message");
+		LOGGER.error("An ERROR Message");
+		
+	}
+
+}
