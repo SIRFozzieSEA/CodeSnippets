8a7c27fb1e4fa36f24b0024ff70dfc3ca361fe37
SIRFozzieSEA <github@codef.com>
2023-06-07 13:06:13 -0700

    Added FourChanTemplateUI

---
+++ src/main/java/com/codef/helpers/FourChanTemplateUI.java
@@ -0,0 +1,101 @@
+package com.codef.helpers;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class FourChanTemplateUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FourChanTemplateUI window = new FourChanTemplateUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FourChanTemplateUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Chan it!");
+		frame.setBounds(100, 100, 951, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		JTextArea inputBox = new JTextArea();
+		inputBox.setBounds(10, 11, 915, 116);
+		inputBox.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		inputBox.setLineWrap(true);
+		frame.getContentPane().add(inputBox);
+
+		JTextArea outputBox = new JTextArea();
+		outputBox.setEditable(false);
+		outputBox.setBounds(10, 172, 915, 378);
+		outputBox.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		frame.getContentPane().add(outputBox);
+
+		JButton doItButton = new JButton("Chan It!");
+		doItButton.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		doItButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				outputBox.setText(doBoldHalfWords(inputBox.getText()));
+			}
+		});
+		doItButton.setBounds(10, 138, 915, 23);
+		frame.getContentPane().add(doItButton);
+
+	}
+
+	private String doBoldHalfWords(String inputString) {
+		String[] words = inputString.split(" ");
+		StringBuilder returnSentence = new StringBuilder();
+
+		if (words[0].equalsIgnoreCase("haha")) {
+			int numOfHas = Integer.valueOf(words[1]).intValue();
+			returnSentence.append("Ah");
+			for (int i = 0; i < numOfHas; i++) {
+				returnSentence.append(" ha");
+			}
+			returnSentence.append(" ... \n\n");
+			returnSentence.append("[breathes in] \n\n");
+			returnSentence.append("... ");
+			for (int i = 0; i < numOfHas; i++) {
+				returnSentence.append(" ha");
+			}
+			returnSentence.append("!\n\n");
+			returnSentence.append("No.");
+			
+		} else {
+			returnSentence.append(inputString);
+		}
+
+		return returnSentence.toString();
+	}
+
+}
