1d61c20c455dfba0ae272c68e3960b1018992ab5
SIRFozzieSEA <github@codef.com>
2023-06-16 23:12:27 -0700

    Added docker stuff, spring boot launcher, interface to use

---
+++ src/main/java/com/codef/codesnippets/SpringBootPageStartup.java
@@ -0,0 +1,29 @@
+package com.codef.codesnippets;
+
+import java.awt.Desktop;
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBootPageStartup {
+	
+	@Value("${launch-page-url}")
+	private String launchPageUrl;
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void launchBrowser() {
+		System.setProperty("java.awt.headless", "false");
+		Desktop desktop = Desktop.getDesktop();
+
+		try {
+			desktop.browse(new URI(launchPageUrl));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
