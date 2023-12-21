cd32fe462c55bdb1aaa56f360f2dcd36a4a5acd3
SIRFozzieSEA <github@codef.com>
2023-12-21 15:07:16 -0800

    Fixing arguments again

--- src/main/java/com/codef/applets/FFMpegTransformer.java
+++ src/main/java/com/codef/applets/FFMpegTransformer.java
@@ -15,16 +15,61 @@ public class FFMpegTransformer {
 
 	private static final boolean ENABLE_MAIN_METHOD = true;
 	private static final Logger LOGGER = LogManager.getLogger(FFMpegTransformer.class.getName());
-
+	
+	private static final String SOURCE_FOLDER = "D:\\Captured\\standalone\\1280x720";
+	private static final String TARGET_FOLDER = SOURCE_FOLDER + "\\final\\";
 	private static final String FFMPEG_BIN_FOLDER = ".\\";
-	private static final String SOURCE_FOLDER = "D:\\Captured\\newer" + "\\";
-	private static final String TARGET_FOLDER = SOURCE_FOLDER + "final\\";
+	
+	private static int totalFiles = 0;
 
 	public static void main(String[] args) {
 
 		if (ENABLE_MAIN_METHOD) {
 			createFileFolder(TARGET_FOLDER);
 			startVisit(SOURCE_FOLDER);
+		}
+		
+		System.out.println("");
+		System.out.println("total files: " + totalFiles);
+
+	}
+
+	public static void visitFileCode(String fileName) {
+
+		if (fileName.toLowerCase().endsWith(".mp4")) {
+
+			String output = "";
+
+			ArrayList<String> commandList = new ArrayList<String>();
+
+			// Calling the FFMPEG with the source file
+			commandList.add(String.format("%sffmpeg.exe -i \"%s%s\"", FFMPEG_BIN_FOLDER, SOURCE_FOLDER + "\\", fileName));
+
+			// Strip off so many seconds of the start of the file
+			// commandList.add("-ss 00:00:08");
+
+			// Filtergraph with scale... the file (width:height), if -1 is used for one if
+			// the values, it will constrain with the original specified values
+			String width = "1280";
+			String height = "720";
+			commandList.add(String.format("-vf scale=%s:%s", width, height));
+
+			// Transpose
+			// “1” = 90 degrees Clockwise
+			// “2” = 90 degrees Counterclockwise,
+			// “3” = 90 Clockwise and Vertical Flip
+			// Values van be chained together (e.g. "transpose=2,transpose=2")
+			// commandList.add("\"transpose=1,transpose=1\"");
+
+			// Specifying the output
+			commandList.add(String.format("\"%s%s\"", TARGET_FOLDER, fileName));
+
+			output = String.join(" ", commandList);
+			System.out.println(output);
+			// LOGGER.info(output);
+			
+			totalFiles++;
+
 		}
 
 	}
@@ -50,45 +95,6 @@ public class FFMpegTransformer {
 		} catch (IOException e) {
 			LOGGER.error(e.toString(), e);
 		}
-	}
-
-	public static void visitFileCode(String fileName) {
-
-		if (fileName.toLowerCase().endsWith(".mp4")) {
-
-			String output = "";
-
-			ArrayList<String> commandList = new ArrayList<String>();
-
-			// Calling the FFMPEG with the source file
-			commandList.add(String.format("%sffmpeg.exe -i \"%s%s\"", FFMPEG_BIN_FOLDER, SOURCE_FOLDER, fileName));
-
-			// Strip off so many seconds of the start of the file
-			commandList.add("-ss 00:00:08");
-
-			// Filtergraph with scale... the file (width:height), if -1 is used for one if
-			// the values,
-			// it will constrain with the original specified values
-			String width = "-1";
-			String height = "720";
-			commandList.add(String.format("-vf scale=%s:%s", width, height));
-
-			// Transpose
-			// “1” = 90 degrees Clockwise
-			// “2” = 90 degrees Counterclockwise,
-			// “3” = 90 Clockwise and Vertical Flip
-			// Values van be chained together (e.g. "transpose=2,transpose=2")
-			// commandList.add("\"transpose=1,transpose=1\"");
-
-			// Specifying the output
-			commandList.add(String.format("\"%s%s\"", TARGET_FOLDER, fileName));
-
-			output = String.join(" ", commandList);
-			System.out.println(output);
-			// LOGGER.info(output);
-
-		}
-
 	}
 
 }
